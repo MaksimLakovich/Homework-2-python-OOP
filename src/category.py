5adc2594c441bf0383b718edc9f59114487ee701
Maksim Lakovich <maks.lakovich@gmail.com>
2024-10-13 15:08:08 +0300

    Доработал классы Product и Category. Добавил счетсики категорий и продуктов. выполнил проверку линтерами (mypy, flake8, black)

--- src/category.py
+++ src/category.py
@@ -1,12 +1,17 @@
 class Category:
-    """Класс Product - шаблон для создания объектов"""
+    """Класс Product - шаблон для создания объекта с данными какой-либо категории под товары"""
 
     name: str
     description: str
-    products: list
+    products: None | list  # Категория товаров может быть создана и без товаров в ней
 
-    def __init__(self, name: str, description: str, products: list) -> None:
+    category_count = 0  # Атрибут класса: подсчет общего количества категорий
+    product_count = 0  # Атрибут класса: подсчет общего количества позиций товаров (не складские остатки)
+
+    def __init__(self, name: str, description: str, products: None | list = None) -> None:
         """Конструктор для инициализации объекта. Т.е. для создания экземпляра класса (объекта)"""
         self.name = name
         self.description = description
-        self.products = products  # ВАРИАНТ С ПУСТЫМ СПИСКОМ
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
--- src/product.py
+++ src/product.py
@@ -1,5 +1,5 @@
 class Product:
-    """Класс Product - шаблон для создания объектов"""
+    """Класс Product - шаблон для создания объекта с данными конкретного товара"""
 
     name: str
     description: str
@@ -12,12 +12,3 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
-
-
-# if __name__ == "__main__":
-#     prod_1 = Product("Nokia", "Орехокол", 10.99, 6)
-#     print(prod_1)
-#     print(prod_1.name)
-#     print(prod_1.description)
-#     print(prod_1.price)
-#     print(prod_1.quantity)
