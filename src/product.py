20164bf15feffab2c556d6a2f88b76a3ea4fa9e8
Maksim Lakovich <maks.lakovich@gmail.com>
2024-10-18 15:39:08 +0300

    Добавлен список existing_products для существующих продуктов для проверки дубликатов и суммирования количества и актуализации цены

--- src/product.py
+++ src/product.py
@@ -17,26 +17,14 @@ class Product:
         self.quantity = quantity
 
     @classmethod
-    def new_product(
-        cls, product_data: Dict[str, Union[str, float, int]], existing_products: List["Product"]
-    ) -> "Product":
+    def new_product(cls, product_data: Dict[str, Union[str, float, int]]) -> "Product":
         """Класс-метод для создания объекта класса Product. Если продукт с таким именем уже существует,
         то обновляется его количество и цена в первоначальной записи.
         :param product_data: Параметры товара в словаре.
-        :param existing_products: Список существующих продуктов для проверки дубликатов.
         :return: Object Product"""
-        # Проверяю наличие продукта с таким же именем и если есть, то обновляю кол-во и устанавливаю максимальную цену
-        for product in existing_products:
-            if product.name == product_data["name"]:
-                product.quantity += int(product_data["quantity"])
-                product.price = max(product.price, float(product_data["price"]))
-                return product
-
         # # Также можно часть кода ниже заменить 1-й строкой с распаковкой словаря: "return cls(**product_data)"
         # # Но такой вариант вызовет ошибки mypy и является менее надежным и уязвимым, если на вход придут какие-либо
         # # иные типы данных или иная структура словаря.
-
-        # Если дубликатов не найдено, создаю новый продукт
         new_product = cls(
             name=str(product_data["name"]),
             description=str(product_data["description"]),
@@ -44,3 +32,28 @@ class Product:
             quantity=int(product_data["quantity"]),
         )
         return new_product
+
+    # # ДОДЕЛАТЬ КЛАСС-МЕТОД НА ПРЕДМЕТ ПРОВЕРКИ ДУБЛЯ ПРОДУКТА И СУММИРОВАНИЯ КОЛИЧЕСТВА
+    # @classmethod
+    # def new_product(
+    #     cls, product_data: Dict[str, Union[str, float, int]], existing_products: List["Product"]
+    # ) -> "Product":
+    #     """Класс-метод для создания объекта класса Product. Если продукт с таким именем уже существует,
+    #     то обновляется его количество и цена в первоначальной записи.
+    #     :param product_data: Параметры товара в словаре.
+    #     :param existing_products: Список существующих продуктов для проверки дубликатов.
+    #     :return: Object Product"""
+    #     # Проверяю наличие продукта с таким же именем и если есть, то обновляю кол-во и устанавливаю максимальную цену
+    #     for product in existing_products:
+    #         if product.name == product_data["name"]:
+    #             product.quantity += int(product_data["quantity"])
+    #             product.price = max(product.price, float(product_data["price"]))
+    #             return product
+    #     # Если дубликатов не найдено, создаю новый продукт
+    #     new_product = cls(
+    #         name=str(product_data["name"]),
+    #         description=str(product_data["description"]),
+    #         price=float(product_data["price"]),
+    #         quantity=int(product_data["quantity"]),
+    #     )
+    #     return new_product
