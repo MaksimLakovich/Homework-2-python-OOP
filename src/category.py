cae085efbbadc34771035ebedacd219d4a9f6665
Maksim Lakovich <maks.lakovich@gmail.com>
2024-10-30 23:29:32 +0300

    В классе Category доработан метод класса add_product(), который добавляет продукт в категорию таким что теперь нельзя добавить вместо продукта или его наследников любой другой объект

--- src/category.py
+++ src/category.py
@@ -35,8 +35,11 @@ class Category:
     def add_product(self, new_product: "Product") -> None:
         """Метод класса для добавления нового продукта в категорию.
         :type new_product: Object Product."""
-        self.__products.append(new_product)
-        Category.product_count += 1
+        if isinstance(new_product, Product):
+            self.__products.append(new_product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self) -> str:
